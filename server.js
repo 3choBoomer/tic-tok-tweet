93a185850dde52f71c0a6468601f42b0d2da72d7
3choboomer <nathancooke@gmail.com>
2018-04-06 00:01:44 -0500

    tweet checker logic

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const path = require('path');
 const authHandler = require('./lib/auth');
 const bodyParser = require('body-parser');
 const config = require('ih-config');
-const scheduler = require('./lib/scheduleTweet')
+const scheduler = require('./lib/scheduleTweet');
+const tweetChecker = require('./lib/tweetChecker');
 
 app.use(bodyParser.json())
 
@@ -16,5 +17,5 @@ app.use(express.static(path.join(__dirname,'/dist')));
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
-
+tweetChecker();
 app.listen(config.get('PORT') || 8000);
